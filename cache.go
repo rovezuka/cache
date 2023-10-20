a4a8bb9fa3d7d5bd56dd733ffd78bd3f268539ba
rovezuka <rostislav.04@list.ru>
2023-10-20 05:46:15 +0300

    Add Time-To-Live for cache

--- cache.go
+++ cache.go
@@ -1,15 +1,35 @@
 package cache
 
+import (
+	"time"
+)
+
 type Cache struct {
 	storage map[string]interface{}
 }
 
 func New() *Cache {
-	return &Cache{make(map[string]interface{})}
+	return &Cache{storage: make(map[string]interface{})}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
+	to := time.After(ttl)
+
 	c.storage[key] = value
+
+	done := make(chan bool, 1)
+
+	go func() {
+		for {
+			<-to
+			done <- true
+			delete(c.storage, key)
+			return
+
+		}
+	}()
+
+	<-done
 }
 
 func (c *Cache) Get(key string) interface{} {
