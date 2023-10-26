b848e4f54fb9c6041b519c70fae332380207a746
rovezuka <rostislav.04@list.ru>
2023-10-26 12:23:17 +0300

    Update work of ttl cache

--- cache.go
+++ cache.go
@@ -1,43 +1,43 @@
 package cache
 
 import (
+	"errors"
+	"fmt"
 	"time"
 )
 
+type value struct {
+	expire int64
+	value  any
+}
+
 type Cache struct {
-	storage map[string]interface{}
+	storage map[string]value
 }
 
 func New() *Cache {
-	return &Cache{storage: make(map[string]interface{})}
+	return &Cache{storage: make(map[string]value)}
 }
 
-func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
-	to := time.After(ttl)
-
-	c.storage[key] = value
-
-	done := make(chan bool, 1)
-
-	go func() {
-		for {
-			<-to
-			done <- true
-			delete(c.storage, key)
-			return
-
-		}
-	}()
-
-	<-done
-}
-
-func (c *Cache) Get(key string) interface{} {
-	value, ok := c.storage[key]
-	if ok {
-		return value
+func (c *Cache) Set(key string, val any, ttl time.Duration) {
+	c.storage[key] = value{
+		expire: time.Now().Unix() + int64(ttl.Seconds()),
+		value:  val,
 	}
-	return ok
+}
+
+func (c *Cache) Get(key string) (any, error) {
+	val, ok := c.storage[key]
+	if ok {
+		if time.Now().Unix() > c.storage[key].expire {
+			delete(c.storage, key)
+			return nil, fmt.Errorf("время жизни значения %s уже завершилось", key)
+		}
+
+		return val.value, nil
+
+	}
+	return nil, errors.New("данного ключа нет в хранилище")
 }
 
 func (c *Cache) Delete(key string) {
